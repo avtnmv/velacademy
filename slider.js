64b1ee4f92c5f478fac3f4b540ea91d86a5bbde2
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-12-23 11:49:03 +0200

    feat: add adaptive

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
 let swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,  
+    slidesPerView: 1,
+    spaceBetween: 10,
     navigation: {
         nextEl: '.custom-next',
         prevEl: '.custom-prev',
@@ -16,23 +16,29 @@ let swiper = new Swiper('.mySwiper', {
         slideChange: function() {
             updateNavButtons(this);
         }
+    },
+    breakpoints: {
+        // От 1000px и выше
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
     }
-  });
-  
-  function updateNavButtons(swiper) {
+});
+
+function updateNavButtons(swiper) {
     const prevButton = document.querySelector('.custom-prev');
     const nextButton = document.querySelector('.custom-next');
-  
+
     if (swiper.isBeginning) {
         prevButton.classList.add('inactive');
     } else {
         prevButton.classList.remove('inactive');
     }
-  
+
     if (swiper.isEnd) {
         nextButton.classList.add('inactive');
     } else {
         nextButton.classList.remove('inactive');
     }
-  }
-  
+}
